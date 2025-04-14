a3a3206f5a6c334d8f763f95d4872aa90e338445
Sahithyan <sahithyan2701@gmail.com>
2025-04-14 13:16:27 +0530

    style: run lint-fix

--- src/TuringMachineSimulator.ts
+++ src/TuringMachineSimulator.ts
@@ -1,217 +1,236 @@
 import { html } from "./utils";
 
 interface ProgramStatement {
-  currentState: string;
-  input: string;
-  nextState: string;
-  output: string;
-  action: "R" | "L" | "H";
+	currentState: string;
+	input: string;
+	nextState: string;
+	output: string;
+	action: "R" | "L" | "H";
 }
 
 class ProgramEditor {
-  statements: ProgramStatement[];
-  activeStatement: ProgramStatement | null = null;
+	statements: ProgramStatement[];
+	activeStatement: ProgramStatement | null = null;
 
-  constructor(initialStatements: Array<ProgramStatement>) {
-    this.statements = initialStatements;
-  }
+	constructor(initialStatements: Array<ProgramStatement>) {
+		this.statements = initialStatements;
+	}
 
-  render(): string {
-    let s = `<div class="program-editor">`;
+	render(): string {
+		let s = `<div class="program-editor">`;
 
-    for (let i = 0; i < this.statements.length; i++) {
-      const statement = this.statements[i];
-      s = s.concat(
-        html`<div class="statement" id="${statement.currentState}-${statement.input}">
+		for (let i = 0; i < this.statements.length; i++) {
+			const statement = this.statements[i];
+			s = s.concat(
+				html`<div class="statement" id="${statement.currentState}-${statement.input}">
           <span>${(i + 1).toString().padStart(2, "0")}</span>
           ${statement.currentState} ${statement.input} : ${statement.nextState} ${statement.output} ${statement.action}
         </div>`,
-      );
-    }
+			);
+		}
 
-    s = s.concat("</div>");
-    return s;
-  }
+		s = s.concat("</div>");
+		return s;
+	}
 
-  findActiveStatement(currentState: string, input: string): [ProgramStatement, number] | null {
-    for (let i = 0; i < this.statements.length; i++) {
-      const statement = this.statements[i];
-      if (statement.currentState === currentState && statement.input === input) {
-        return [statement, i];
-      }
-    }
-    return null;
-  }
+	findActiveStatement(
+		currentState: string,
+		input: string,
+	): [ProgramStatement, number] | null {
+		for (let i = 0; i < this.statements.length; i++) {
+			const statement = this.statements[i];
+			if (
+				statement.currentState === currentState &&
+				statement.input === input
+			) {
+				return [statement, i];
+			}
+		}
+		return null;
+	}
 
-  highlightActiveStatement(container: HTMLElement, statementIndex: number) {
-    this.activeStatement = null;
-    const previousActiveStatement = container.querySelector(".program-editor .statement.active");
-    if (previousActiveStatement) {
-      previousActiveStatement.classList.remove("active");
-    }
+	highlightActiveStatement(container: HTMLElement, statementIndex: number) {
+		this.activeStatement = null;
+		const previousActiveStatement = container.querySelector(
+			".program-editor .statement.active",
+		);
+		if (previousActiveStatement) {
+			previousActiveStatement.classList.remove("active");
+		}
 
-    const statement = this.statements[statementIndex];
-    this.activeStatement = statement;
-    const statementElement = container.querySelector(`.statement#${statement.currentState}-${statement.input}`);
-    if (statementElement) {
-      statementElement.classList.add("active");
-    }
-  }
+		const statement = this.statements[statementIndex];
+		this.activeStatement = statement;
+		const statementElement = container.querySelector(
+			`.statement#${statement.currentState}-${statement.input}`,
+		);
+		if (statementElement) {
+			statementElement.classList.add("active");
+		}
+	}
 }
 
 class Tape {
-  value: string[];
+	value: string[];
 
-  constructor(initialValue: string[]) {
-    this.value = initialValue;
-  }
+	constructor(initialValue: string[]) {
+		this.value = initialValue;
+	}
 
-  stringify(): string {
-    return this.value.join(",");
-  }
-  read(position: number) {
-    return this.value[position];
-  }
+	stringify(): string {
+		return this.value.join(",");
+	}
+	read(position: number) {
+		return this.value[position];
+	}
 
-  write(container: HTMLElement, position: number, value: string) {
-    this.value[position] = value;
-    const element = container.querySelector(`#tape-cell-${position}`);
-    if (element) {
-      element.innerHTML = value;
-    }
-  }
+	write(container: HTMLElement, position: number, value: string) {
+		this.value[position] = value;
+		const element = container.querySelector(`#tape-cell-${position}`);
+		if (element) {
+			element.innerHTML = value;
+		}
+	}
 
-  render(headPosition: number) {
-    let s = "";
-    for (let i = 0; i < this.value.length; i++) {
-      if (i === headPosition) {
-        s = s.concat(`<div class="cell head" id="tape-cell-${i}">${this.value[i]}</div>`);
-      } else {
-        s = s.concat(`<div class="cell" id="tape-cell-${i}">${this.value[i]}</div>`);
-      }
-    }
-    return s;
-  }
+	render(headPosition: number) {
+		let s = "";
+		for (let i = 0; i < this.value.length; i++) {
+			if (i === headPosition) {
+				s = s.concat(
+					`<div class="cell head" id="tape-cell-${i}">${this.value[i]}</div>`,
+				);
+			} else {
+				s = s.concat(
+					`<div class="cell" id="tape-cell-${i}">${this.value[i]}</div>`,
+				);
+			}
+		}
+		return s;
+	}
 
-  updateHeadPosition(container: HTMLElement, newHeadPosition: number) {
-    const previousHead = container.querySelector(".tape .cell.head");
-    if (previousHead) {
-      previousHead.classList.remove("head");
-    }
+	updateHeadPosition(container: HTMLElement, newHeadPosition: number) {
+		const previousHead = container.querySelector(".tape .cell.head");
+		if (previousHead) {
+			previousHead.classList.remove("head");
+		}
 
-    const newHead = container.querySelector(`#tape-cell-${newHeadPosition}`);
-    if (newHead) {
-      newHead.classList.add("head");
-    }
-  }
+		const newHead = container.querySelector(`#tape-cell-${newHeadPosition}`);
+		if (newHead) {
+			newHead.classList.add("head");
+		}
+	}
 }
 
-function generateExplanationFromStatement(statement: ProgramStatement, index: number): string {
-  return html`Based on these, the machine will execute the statement
+function generateExplanationFromStatement(
+	statement: ProgramStatement,
+	index: number,
+): string {
+	return html`Based on these, the machine will execute the statement
     <span class="font-mono">${(index + 1).toString().padStart(2, "0")}</span>.<br />
     The next state will be <span class="font-mono">${statement.nextState}</span>.<br />
     "${statement.output}" will be written to the tape. After that,
-    ${statement.action === "H"
-      ? "the machine will halt."
-      : `the head will move <span class="font-mono">${statement.action === "R" ? "right" : "left"}</span>.`}`;
+    ${
+			statement.action === "H"
+				? "the machine will halt."
+				: `the head will move <span class="font-mono">${statement.action === "R" ? "right" : "left"}</span>.`
+		}`;
 }
 
 export class TuringMachineSimulator {
-  container: HTMLElement;
-  currentState: string;
-  currentTape: Tape;
-  headPosition: number;
-  editor: ProgramEditor;
-  isRunning: boolean;
+	container: HTMLElement;
+	currentState: string;
+	currentTape: Tape;
+	headPosition: number;
+	editor: ProgramEditor;
+	isRunning: boolean;
 
-  constructor(container: HTMLElement) {
-    this.container = container;
-    this.currentState = "Q0";
-    this.currentTape = new Tape(["b", "b", "b", "b", "1", "1", "1", "b", "b"]);
-    this.headPosition = 4;
-    this.isRunning = true;
+	constructor(container: HTMLElement) {
+		this.container = container;
+		this.currentState = "Q0";
+		this.currentTape = new Tape(["b", "b", "b", "b", "1", "1", "1", "b", "b"]);
+		this.headPosition = 4;
+		this.isRunning = true;
 
-    this.editor = new ProgramEditor([
-      {
-        currentState: "Q0",
-        input: "1",
-        nextState: "Q1",
-        output: "X",
-        action: "L",
-      },
-      {
-        currentState: "Q0",
-        input: "X",
-        nextState: "Q0",
-        output: "X",
-        action: "R",
-      },
-      {
-        currentState: "Q0",
-        input: "Y",
-        nextState: "Q0",
-        output: "Y",
-        action: "R",
-      },
-      {
-        currentState: "Q1",
-        input: "X",
-        nextState: "Q1",
-        output: "X",
-        action: "L",
-      },
-      {
-        currentState: "Q1",
-        input: "Y",
-        nextState: "Q1",
-        output: "Y",
-        action: "L",
-      },
-      {
-        currentState: "Q1",
-        input: "b",
-        nextState: "Q0",
-        output: "Y",
-        action: "R",
-      },
-      {
-        currentState: "Q0",
-        input: "b",
-        nextState: "Q2",
-        output: "b",
-        action: "L",
-      },
-      {
-        currentState: "Q2",
-        input: "X",
-        nextState: "Q2",
-        output: "1",
-        action: "L",
-      },
-      {
-        currentState: "Q2",
-        input: "Y",
-        nextState: "Q2",
-        output: "1",
-        action: "L",
-      },
-      {
-        currentState: "Q2",
-        input: "b",
-        nextState: "Q2",
-        output: "b",
-        action: "R",
-      },
-      {
-        currentState: "Q2",
-        input: "1",
-        nextState: "Q2",
-        output: "1",
-        action: "H",
-      },
-    ]);
+		this.editor = new ProgramEditor([
+			{
+				currentState: "Q0",
+				input: "1",
+				nextState: "Q1",
+				output: "X",
+				action: "L",
+			},
+			{
+				currentState: "Q0",
+				input: "X",
+				nextState: "Q0",
+				output: "X",
+				action: "R",
+			},
+			{
+				currentState: "Q0",
+				input: "Y",
+				nextState: "Q0",
+				output: "Y",
+				action: "R",
+			},
+			{
+				currentState: "Q1",
+				input: "X",
+				nextState: "Q1",
+				output: "X",
+				action: "L",
+			},
+			{
+				currentState: "Q1",
+				input: "Y",
+				nextState: "Q1",
+				output: "Y",
+				action: "L",
+			},
+			{
+				currentState: "Q1",
+				input: "b",
+				nextState: "Q0",
+				output: "Y",
+				action: "R",
+			},
+			{
+				currentState: "Q0",
+				input: "b",
+				nextState: "Q2",
+				output: "b",
+				action: "L",
+			},
+			{
+				currentState: "Q2",
+				input: "X",
+				nextState: "Q2",
+				output: "1",
+				action: "L",
+			},
+			{
+				currentState: "Q2",
+				input: "Y",
+				nextState: "Q2",
+				output: "1",
+				action: "L",
+			},
+			{
+				currentState: "Q2",
+				input: "b",
+				nextState: "Q2",
+				output: "b",
+				action: "R",
+			},
+			{
+				currentState: "Q2",
+				input: "1",
+				nextState: "Q2",
+				output: "1",
+				action: "H",
+			},
+		]);
 
-    this.container.innerHTML = html`
+		this.container.innerHTML = html`
       <div class="grid grid-cols-1 lg:grid-cols-[400px_1fr] h-screen">
         <section class="lg:bg-stone-900/40 h-fit lg:h-full px-3 py-2">
           <h1 class="mb-4 font-semibold text-2xl">Turing Machine Simulator</h1>
@@ -255,119 +274,131 @@ export class TuringMachineSimulator {
       </div>
     `;
 
-    this.addEventListeners();
-    this.renderExplanation();
-    this.highlightActiveStatement();
-  }
+		this.addEventListeners();
+		this.renderExplanation();
+		this.highlightActiveStatement();
+	}
 
-  highlightActiveStatement() {
-    const activeStatement = this.editor.findActiveStatement(
-      this.currentState,
-      this.currentTape.read(this.headPosition),
-    );
-    if (activeStatement != null) {
-      this.editor.highlightActiveStatement(this.container, activeStatement[1]);
-    }
-  }
+	highlightActiveStatement() {
+		const activeStatement = this.editor.findActiveStatement(
+			this.currentState,
+			this.currentTape.read(this.headPosition),
+		);
+		if (activeStatement != null) {
+			this.editor.highlightActiveStatement(this.container, activeStatement[1]);
+		}
+	}
 
-  renderExplanation() {
-    const explanationElement = this.container.querySelector<HTMLParagraphElement>("#explanation");
-    if (!explanationElement) return;
+	renderExplanation() {
+		const explanationElement =
+			this.container.querySelector<HTMLParagraphElement>("#explanation");
+		if (!explanationElement) return;
 
-    if (!this.isRunning) {
-      explanationElement.innerHTML = "The machine has halted.";
-      return;
-    }
+		if (!this.isRunning) {
+			explanationElement.innerHTML = "The machine has halted.";
+			return;
+		}
 
-    const activeStatement = this.editor.findActiveStatement(
-      this.currentState,
-      this.currentTape.read(this.headPosition),
-    );
+		const activeStatement = this.editor.findActiveStatement(
+			this.currentState,
+			this.currentTape.read(this.headPosition),
+		);
 
-    explanationElement.innerHTML = `The machine is in state <b class="font-mono">${this.currentState}</b>. The current input is
+		explanationElement.innerHTML = `The machine is in state <b class="font-mono">${this.currentState}</b>. The current input is
     <b class="font-mono">${this.currentTape.read(this.headPosition)}</b>.
     ${
-      activeStatement == null
-        ? "No statements match the current state and input."
-        : generateExplanationFromStatement(activeStatement[0], activeStatement[1])
-    }`;
-  }
+			activeStatement == null
+				? "No statements match the current state and input."
+				: generateExplanationFromStatement(
+						activeStatement[0],
+						activeStatement[1],
+					)
+		}`;
+	}
 
-  halt() {
-    this.isRunning = false;
-    const nextBtn = this.container.querySelector("#next-btn");
-    if (nextBtn instanceof HTMLButtonElement) {
-      nextBtn.disabled = true;
-    }
-    this.renderExplanation();
-  }
+	halt() {
+		this.isRunning = false;
+		const nextBtn = this.container.querySelector("#next-btn");
+		if (nextBtn instanceof HTMLButtonElement) {
+			nextBtn.disabled = true;
+		}
+		this.renderExplanation();
+	}
 
-  nextStep() {
-    if (!this.editor.activeStatement) return;
+	nextStep() {
+		if (!this.editor.activeStatement) return;
 
-    this.currentState = this.editor.activeStatement.nextState;
-    const currentStateEl = this.container.querySelector("#current-state");
-    if (currentStateEl) {
-      currentStateEl.textContent = this.currentState;
-    }
+		this.currentState = this.editor.activeStatement.nextState;
+		const currentStateEl = this.container.querySelector("#current-state");
+		if (currentStateEl) {
+			currentStateEl.textContent = this.currentState;
+		}
 
-    this.currentTape.write(this.container, this.headPosition, this.editor.activeStatement.output);
+		this.currentTape.write(
+			this.container,
+			this.headPosition,
+			this.editor.activeStatement.output,
+		);
 
-    switch (this.editor.activeStatement.action) {
-      case "L":
-        this.headPosition -= 1;
-        break;
-      case "R":
-        this.headPosition += 1;
-        break;
-      case "H": {
-        this.halt();
-        return;
-      }
-    }
-    this.currentTape.updateHeadPosition(this.container, this.headPosition);
+		switch (this.editor.activeStatement.action) {
+			case "L":
+				this.headPosition -= 1;
+				break;
+			case "R":
+				this.headPosition += 1;
+				break;
+			case "H": {
+				this.halt();
+				return;
+			}
+		}
+		this.currentTape.updateHeadPosition(this.container, this.headPosition);
 
-    this.highlightActiveStatement();
-    this.renderExplanation();
-  }
+		this.highlightActiveStatement();
+		this.renderExplanation();
+	}
 
-  rebuildMachine() {
-    const initialTapeInputElement = this.container.querySelector("#initial-tape");
-    if (!(initialTapeInputElement instanceof HTMLInputElement)) return;
+	rebuildMachine() {
+		const initialTapeInputElement =
+			this.container.querySelector("#initial-tape");
+		if (!(initialTapeInputElement instanceof HTMLInputElement)) return;
 
-    const initialStateInputElement = this.container.querySelector("#initial-state");
-    if (!(initialStateInputElement instanceof HTMLInputElement)) return;
+		const initialStateInputElement =
+			this.container.querySelector("#initial-state");
+		if (!(initialStateInputElement instanceof HTMLInputElement)) return;
 
-    const initialHeadPositionInputElement = this.container.querySelector("#initial-head-position");
-    if (!(initialHeadPositionInputElement instanceof HTMLInputElement)) return;
+		const initialHeadPositionInputElement = this.container.querySelector(
+			"#initial-head-position",
+		);
+		if (!(initialHeadPositionInputElement instanceof HTMLInputElement)) return;
 
-    const initialTape = initialTapeInputElement.value;
-    const initialState = initialStateInputElement.value;
-    const initialHeadPosition = initialHeadPositionInputElement.valueAsNumber;
+		const initialTape = initialTapeInputElement.value;
+		const initialState = initialStateInputElement.value;
+		const initialHeadPosition = initialHeadPositionInputElement.valueAsNumber;
 
-    this.headPosition = initialHeadPosition - 1;
+		this.headPosition = initialHeadPosition - 1;
 
-    this.currentTape.value = initialTape.split(",");
+		this.currentTape.value = initialTape.split(",");
 
-    const tape = this.container.querySelector(".tape");
-    if (tape) {
-      tape.innerHTML = this.currentTape.render(this.headPosition);
-    }
+		const tape = this.container.querySelector(".tape");
+		if (tape) {
+			tape.innerHTML = this.currentTape.render(this.headPosition);
+		}
 
-    this.currentState = initialState;
-    this.highlightActiveStatement();
-    this.renderExplanation();
-  }
+		this.currentState = initialState;
+		this.highlightActiveStatement();
+		this.renderExplanation();
+	}
 
-  addEventListeners() {
-    const nextBtn = this.container.querySelector("#next-btn");
-    if (nextBtn instanceof HTMLButtonElement) {
-      nextBtn.addEventListener("click", () => this.nextStep());
-    }
+	addEventListeners() {
+		const nextBtn = this.container.querySelector("#next-btn");
+		if (nextBtn instanceof HTMLButtonElement) {
+			nextBtn.addEventListener("click", () => this.nextStep());
+		}
 
-    const rebuildBtn = this.container.querySelector("#rebuild-machine-btn");
-    if (rebuildBtn instanceof HTMLButtonElement) {
-      rebuildBtn.addEventListener("click", () => this.rebuildMachine());
-    }
-  }
+		const rebuildBtn = this.container.querySelector("#rebuild-machine-btn");
+		if (rebuildBtn instanceof HTMLButtonElement) {
+			rebuildBtn.addEventListener("click", () => this.rebuildMachine());
+		}
+	}
 }
